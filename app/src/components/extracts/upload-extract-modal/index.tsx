1d09fb0ffd83f1a64244bc29e14269fe88fc6455
Helder Nicollas Leão Peixoto <nicollashelder@gmail.com>
2025-08-14 16:36:52 -0300

    feat: add upload logic to upload extract modal

--- app/src/components/extracts/upload-extract-modal/index.tsx
+++ app/src/components/extracts/upload-extract-modal/index.tsx
@@ -9,7 +9,7 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Upload } from "lucide-react";
+import { Upload, X } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { FormSchema, formSchema } from "./schema";
 import { useMutation } from "@tanstack/react-query";
@@ -17,7 +17,7 @@ import { fetcher } from "@/lib/fetcher";
 import { toFormData } from "@/lib/to-form-data";
 
 const uploadFile = async (data: FormSchema) => {
-  await fetcher("/app/upload-extrato", {
+  await fetcher("/app/extrato-bancario", {
     method: "POST",
     body: toFormData(data),
     headers: {
@@ -43,23 +43,29 @@ export function UploadExtractModal({
     register,
     handleSubmit,
     watch,
+    setValue,
+    reset,
     formState: { errors },
   } = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
-    mode: "onSubmit",
-    reValidateMode: "onSubmit",
+    defaultValues: {
+      extractFile: null,
+    },
   });
 
   const file = watch("extractFile");
 
-  const submitForm = (data: FormSchema) => {
-    mutate(data);
+  const submitForm = (data: FormSchema) => mutate(data);
+
+  const handleClose = () => {
+    reset();
+    onOpenChange();
   };
 
   return (
-    <Dialog open={open} onOpenChange={onOpenChange}>
-      <form onSubmit={handleSubmit(submitForm)}>
-        <DialogContent className="sm:max-w-[550px]">
+    <Dialog open={open} onOpenChange={handleClose}>
+      <DialogContent className="sm:max-w-[550px]">
+        <form onSubmit={handleSubmit(submitForm)}>
           <DialogHeader>
             <DialogTitle>Adicionar um novo extrato</DialogTitle>
             <DialogDescription>
@@ -67,29 +73,57 @@ export function UploadExtractModal({
             </DialogDescription>
           </DialogHeader>
 
-          <div className="">
+          <div className="py-3">
             <label
-              className="border-dashed border-2 rounded-md h-56 flex items-center justify-center cursor-pointer"
+              className="border-dashed border-2 rounded-md h-56 items-center justify-center cursor-pointer data-[visible=true]:flex hidden group hover:bg-blue-500/20 transition-colors duration-300 data-[error=true]:border-red-600"
               htmlFor="extractFile"
+              data-visible={!file}
+              data-error={!!errors?.extractFile?.message}
             >
-              <Upload className="text-gray-500" />
+              <Upload className="text-gray-500 group-hover:hidden" />
+              <span className="hidden text-gray-400 group-hover:block">
+                Clique e selecione o arquivo, ou arraste ele para essa área.
+              </span>
             </label>
             <input
               type="file"
               id="extractFile"
               className="hidden"
-              {...register("extractFile")}
+              {...register("extractFile", {
+                onChange: (e) => setValue("extractFile", e.target.files[0]),
+              })}
             />
+
+            <div
+              className="border-2 border-blue-500 bg-blue-500/30 p-2 rounded-md justify-between data-[visible=true]:flex hidden data-[error=true]:border-red-600"
+              data-error={!!errors?.extractFile?.message}
+              data-visible={!!file}
+            >
+              <span className="text-blue-600">
+                {file?.name} - {(file?.size / 1024).toFixed(1)}KB
+              </span>
+              <button
+                type="button"
+                onClick={() => setValue("extractFile", null)}
+              >
+                <X className="text-blue-600" />
+              </button>
+            </div>
+            <span className="text-red-600 text-xs">
+              {errors?.extractFile?.message}
+            </span>
           </div>
 
           <DialogFooter>
             <DialogClose asChild>
-              <Button variant="outline">Cancelar</Button>
+              <Button variant="outline" type="button">
+                Cancelar
+              </Button>
             </DialogClose>
             <Button type="submit">Enviar</Button>
           </DialogFooter>
-        </DialogContent>
-      </form>
+        </form>
+      </DialogContent>
     </Dialog>
   );
 }
--- app/src/components/extracts/upload-extract-modal/schema.ts
+++ app/src/components/extracts/upload-extract-modal/schema.ts
@@ -2,15 +2,16 @@ import { z } from "zod";
 
 export const formSchema = z.object({
   extractFile: z
-    .instanceof(FileList)
-    .transform((list) => list.item(0))
+    .instanceof(File)
+    .nullish()
+    .refine((file) => !!file, "O arquivo é obrigatório")
     .refine(
-      (file) => file!.size <= 5 * 1024 * 1024,
+      (file) => file?.size <= 5 * 1024 * 1024,
       "O arquivo deve ter no máximo 5mb"
     )
     .refine((file) => {
       const allowedExtensions = [".ofx"];
-      const fileExtension = "." + file.name.split(".").pop().toLowerCase();
+      const fileExtension = "." + file?.name?.split(".")?.pop()?.toLowerCase();
       return allowedExtensions.includes(fileExtension);
     }, "O arquivo deve ser do tipo .ofx"),
 });
