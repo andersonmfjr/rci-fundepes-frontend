7e3ce19bdf202ae7e2339f558c52465a1ea2a960
Helder Nicollas Leão Peixoto <nicollashelder@gmail.com>
2025-09-10 11:34:09 -0300

    feat: extract details skeleton

--- app/src/components/extracts/extract-details-modal/index.tsx
+++ app/src/components/extracts/extract-details-modal/index.tsx
@@ -1,11 +1,10 @@
+import { ExtractDetailsSkeleton } from "@/components/skeletons/extracts-skeleton";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
-  DialogClose,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
@@ -23,7 +22,7 @@ import { useSearchParams } from "react-router-dom";
 
 export function ExtractDetailsModal() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const { data: extract } = useQuery({
+  const { data: extract, isLoading } = useQuery({
     queryKey: ["get-extract", searchParams.get("current")],
     queryFn: async ({ queryKey: [_, id] }) =>
       fetcher<BankExtract>(`/app/extrato-bancario/${id}`),
@@ -47,59 +46,62 @@ export function ExtractDetailsModal() {
               Na tabela estão as tranferências contidas no extrato.
             </DialogDescription>
           </DialogHeader>
-          <div className="w-full">
-            <div className="flex flex-col gap-2">
-              <div className="">
-                <span className="text-xs font-semibold">Descrição:</span>
-                <p>{extract?.descricao}</p>
+          {isLoading && <ExtractDetailsSkeleton />}
+          {!isLoading && (
+            <div className="w-full">
+              <div className="flex flex-col gap-2">
+                <div className="">
+                  <span className="text-xs font-semibold">Descrição:</span>
+                  <p>{extract?.descricao}</p>
+                </div>
+                <div className="">
+                  <span className="text-xs font-semibold">Mês/Ano:</span>
+                  <p>
+                    {extract?.mes_referencia}/{extract?.ano_referencia}
+                  </p>
+                </div>
+                <div className="">
+                  <span className="text-xs font-semibold">Conta:</span>
+                  <p>
+                    {extract?.mes_referencia}/{extract?.ano_referencia}
+                  </p>
+                </div>
+                <div className="">
+                  <span className="text-xs font-semibold block mb-1">
+                    Status:
+                  </span>
+                  {extract?.processado ? (
+                    <Badge className="bg-emerald-600">Processado</Badge>
+                  ) : (
+                    <Badge className="bg-orange-500">Em processamento</Badge>
+                  )}
+                </div>
               </div>
-              <div className="">
-                <span className="text-xs font-semibold">Mês/Ano:</span>
-                <p>
-                  {extract?.mes_referencia}/{extract?.ano_referencia}
-                </p>
-              </div>
-              <div className="">
-                <span className="text-xs font-semibold">Conta:</span>
-                <p>
-                  {extract?.mes_referencia}/{extract?.ano_referencia}
-                </p>
-              </div>
-              <div className="">
-                <span className="text-xs font-semibold block mb-1">
-                  Status:
-                </span>
-                {extract?.processado ? (
-                  <Badge className="bg-emerald-600">Processado</Badge>
-                ) : (
-                  <Badge className="bg-orange-500">Em processamento</Badge>
-                )}
-              </div>
-            </div>
 
-            <hr className="my-3" />
+              <hr className="my-3" />
 
-            <div className="overflow-hidden">
               <h2 className="font-semibold mb-3">Transferências</h2>
-              <Table className="min-w-full">
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="max-w-10">Data</TableHead>
-                    <TableHead className="max-w-10">Valor</TableHead>
-                    <TableHead className="max-w-10">Observação</TableHead>
-                    <TableHead className="max-w-10">Conta projeto</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  <TableRow>
-                    <TableCell colSpan={4} className="text-center">
-                      Nenhuma transferência foi realizada
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
+              <div className="overflow-hidden rounded-md border bg-white">
+                <Table className="min-w-full">
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="max-w-10">Data</TableHead>
+                      <TableHead className="max-w-10">Valor</TableHead>
+                      <TableHead className="max-w-10">Observação</TableHead>
+                      <TableHead className="max-w-10">Conta projeto</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell colSpan={4} className="text-center">
+                        Nenhuma transferência foi realizada
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </div>
             </div>
-          </div>
+          )}
         </DialogContent>
       </form>
     </Dialog>
